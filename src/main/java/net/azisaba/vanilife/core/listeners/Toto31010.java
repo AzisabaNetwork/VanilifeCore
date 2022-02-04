a12509560328efab6e68c719a6730a65238d2b72
acrylic-style <me@acrylicstyle.xyz>
2022-02-05 01:18:09 +0900

    add new super secret config

--- src/main/java/net/azisaba/vanilife/core/listeners/Toto31010.java
+++ src/main/java/net/azisaba/vanilife/core/listeners/Toto31010.java
@@ -1,13 +1,20 @@
 package net.azisaba.vanilife.core.listeners;
 
+import net.azisaba.vanilife.core.VanilifeCore;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class Toto31010 implements Listener {
+    private final VanilifeCore plugin;
+
+    public Toto31010(VanilifeCore plugin) {
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
-        if (e.getPlayer().getUniqueId().toString().equals("1865ab8c-700b-478b-9b52-a8c58739df1a")) {
+        if (plugin.getConfig().getBoolean("toto31010", false) || e.getPlayer().getUniqueId().toString().equals("1865ab8c-700b-478b-9b52-a8c58739df1a")) {
             e.setMessage(e.getMessage() + "///");
         }
     }
