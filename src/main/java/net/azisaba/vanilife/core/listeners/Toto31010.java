b896ca3618355970d7257f4717c2170c5ca9146c
acrylic-style <me@acrylicstyle.xyz>
2022-02-05 20:12:57 +0900

    Turn off with no-toto31010

--- src/main/java/net/azisaba/vanilife/core/listeners/Toto31010.java
+++ src/main/java/net/azisaba/vanilife/core/listeners/Toto31010.java
@@ -14,6 +14,7 @@ public class Toto31010 implements Listener {
 
     @EventHandler
     public void onAsyncPlayerChat(AsyncPlayerChatEvent e) {
+        if (plugin.getConfig().getBoolean("no-toto31010", true)) return;
         if (plugin.getConfig().getBoolean("toto31010", false) || e.getPlayer().getUniqueId().toString().equals("1865ab8c-700b-478b-9b52-a8c58739df1a")) {
             e.setMessage(e.getMessage() + "///");
         }
