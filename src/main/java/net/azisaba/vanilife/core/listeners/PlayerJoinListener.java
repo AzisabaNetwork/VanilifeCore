28fee7ad3d5f45acb044deaaac12a5cbf866218e
meru <merunno92@gmail.com>
2022-02-20 20:25:12 +0900

    Update src/main/java/net/azisaba/vanilife/core/listeners/PlayerJoinListener.java
    Co-authored-by: Acrylic Style <me@acrylicstyle.xyz>

--- src/main/java/net/azisaba/vanilife/core/listeners/PlayerJoinListener.java
+++ src/main/java/net/azisaba/vanilife/core/listeners/PlayerJoinListener.java
@@ -7,7 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-public record PlayerJoinListener(VanilifeCore plugin) implements Listener {
+public class PlayerJoinListener implements Listener {
 
   @EventHandler
   public void onJoin(PlayerJoinEvent e) {
