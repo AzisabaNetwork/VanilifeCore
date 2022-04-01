cbd16fc1584842a20ec1ba6cd3dd453c91612dd8
merunno <merunno92@gmail.com>
2022-04-01 13:10:14 +0900

    feat: remove import

--- src/main/java/net/azisaba/vanilife/core/util/VaniLifeBook.java
+++ src/main/java/net/azisaba/vanilife/core/util/VaniLifeBook.java
@@ -1,14 +1,10 @@
 package net.azisaba.vanilife.core.util;
 
-import net.kyori.adventure.text.Component;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.awt.*;
-import java.util.Arrays;
 
 public record VaniLifeBook(JavaPlugin plugin) {
 
