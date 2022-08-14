d739baeab90d904eb89a9efef51e84189b6c454d
meru <merunno92@gmail.com>
2022-08-14 16:20:38 +0900

    feat: !が先頭にあるメッセージはAdminChatから除外するように (#11)

--- src/main/java/net/azisaba/vanilife/core/listeners/AdminChatListener.java
+++ src/main/java/net/azisaba/vanilife/core/listeners/AdminChatListener.java
@@ -3,6 +3,7 @@ package net.azisaba.vanilife.core.listeners;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.azisaba.vanilife.core.VanilifeCore;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,6 +12,10 @@ public record AdminChatListener(VanilifeCore plugin) implements Listener {
   @EventHandler
   public void onAsyncChat(AsyncChatEvent e) {
     if (plugin.getAdminChatUtil().isAdminChatEnabled(e.getPlayer())) {
+      if (PlainTextComponentSerializer.plainText().serialize(e.message()).startsWith("!")) {
+        return;
+      }
+
       e.setCancelled(true);
       String messageAsString = LegacyComponentSerializer.legacySection().serialize(e.message());
       Bukkit.getScheduler().runTask(plugin, () -> plugin.getAdminChatUtil().handleAdminChat(
